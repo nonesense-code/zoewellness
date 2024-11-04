4c199db1b8a06d6d636befc06011f0946bf4dc28
kaushal <kaushalkadariya2062@gmail.com>
2024-11-04 17:49:43 +0545

    updated codes

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const path = require("path");
 const Product = require("./models/Products");
 
