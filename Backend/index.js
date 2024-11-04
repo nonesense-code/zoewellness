6593baf3dfd40df1a0dee860432b9531cd4a05a7
kaushal <kaushalkadariya2062@gmail.com>
2024-11-04 17:45:18 +0545

    updated codes

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", async (req, res) => {
   try {
     const products = await Product.find();
