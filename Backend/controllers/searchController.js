72160011d77c20962994b954d82e8d68593f8a7c
kaushal <kaushalkadariya2062@gmail.com>
2024-11-05 22:59:16 +0545

    updated

--- Backend/controllers/searchController.js
+++ Backend/controllers/searchController.js
@@ -10,6 +10,9 @@ module.exports.searchProducts = async (req, res) => {
   const normalizedInput = search_items.replace(/\s+/g, "").toLowerCase();
 
   try {
+    const allowedCookie = req.cookies.allowed;
+    const cookieExist = allowedCookie !== undefined && allowedCookie !== "";
+
     const products = await Product.aggregate([
       {
         $addFields: {
@@ -27,7 +30,7 @@ module.exports.searchProducts = async (req, res) => {
       },
     ]);
 
-    res.render("Home", { items: products });
+    res.render("Home", { items: products, cookieExist });
   } catch (error) {
     console.error("Error fetching product:", error);
     res.status(500).send("Server error.");
